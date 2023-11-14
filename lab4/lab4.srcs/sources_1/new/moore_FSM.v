fd0a7f0894ff83e798334595e4a2f903b7df8e42
Ethan Levine <101854509+EthanL3@users.noreply.github.com>
2023-11-14 15:36:41 -0500

    Update moore_FSM.v

--- lab4/lab4.srcs/sources_1/new/moore_FSM.v
+++ lab4/lab4.srcs/sources_1/new/moore_FSM.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
 
-module moore_FSM(out, clk, reset, read, in);
-    input clk, reset, read; 
-    input [7:0] in;
-    output reg out;
-    
+module moore_FSM(
+    output reg out,
+    input clk,
+    input reset,
+    input read,
+    input button,
+    input [7:0] in
+);
     reg [2:0] p;
     reg [2:0] present_state, next_state;
     
@@ -12,29 +15,47 @@ module moore_FSM(out, clk, reset, read, in);
     
     initial present_state = S3;
     
-    always @(reset or read or present_state) begin
-        case(present_state)
-            S0: if(reset)              
-                    next_state = S3;
-                else if(read && !reset)           
+    always @(posedge clk or posedge reset or posedge read or posedge button) begin
+        if (reset)
+            next_state = S3;
+        else
+            next_state = present_state;
+
+        case (present_state)
+            S0: begin
+                if (read && !reset)
                     next_state = S1;
                 else if (!read && !reset)
                     next_state = S0;
-            S1: next_state = S2;
-            S2: next_state = S0;
-            S3: next_state = S0;  
+            end
+            S1: begin
+                if (button && !reset)
+                    next_state = S2;
+                else
+                    next_state = S1;
+            end
+            S2: begin
+                if (!button && !reset)
+                    next_state = S0;
+                else
+                    next_state = S2;
+            end
+            S3: begin
+                if (!reset)
+                    next_state = S0;
+            end
         endcase
     end 
+
     always @(posedge clk) begin
         present_state <= next_state;
-        case(present_state)
+        case (present_state)
             S1: out <= in[p];
             S2: p <= p + 1;
             S3: begin
                 p <= 0;
                 out <= 0;
-                end
+            end
         endcase
     end
 endmodule
-
