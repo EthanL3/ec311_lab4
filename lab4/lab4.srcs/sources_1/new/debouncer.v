68493161e0dad19e0f4fe951072b38c9983621fe
Ethan Levine <101854509+EthanL3@users.noreply.github.com>
2023-11-14 15:35:34 -0500

    Update debouncer.v

--- lab4/lab4.srcs/sources_1/new/debouncer.v
+++ lab4/lab4.srcs/sources_1/new/debouncer.v
@@ -2,9 +2,7 @@ module debouncer(
     input wire clk,     
     input wire rst,     
     input wire button,  //Button input
-    output reg debounced_button,  //Debounced button output
-    input [7:0] in,
-    output out
+    output reg debounced_button  //Debounced button output
 );
 reg [1:0] state, next_state;
 // Parameter for debounce delay (adjust this based on your clock frequency)
@@ -17,38 +15,40 @@ always @(posedge clk or posedge rst) begin
         debounce_counter <= 16'b0;
     end else begin
         state <= next_state;
-        debounce_counter <= debounce_counter + 1;
+        if (debounce_counter == DELAY_COUNT)
+            debounce_counter <= 16'b0;
+        else
+            debounce_counter <= debounce_counter + 1;
     end
 end
-//add state logic with IDLE being 2'b00, PRESSED being 2'b01, and RELEASED as 2'b10
-parameter i = 2'b00, p = 2'b01, r = 2'b10;
+
 always @(posedge clk or posedge rst) begin
     case (state)
         2'b00: begin // IDLE state
             if (button == 1'b0) begin
-                next_state <= i; //fill in missing next state
+                next_state <= 2'b01; // Move to PRESSED state
             end else begin
-                next_state <= p;//fill in missing next state
+                next_state <= 2'b00; // Stay in IDLE state
             end
         end
 
         2'b01: begin // PRESSED state
             if (button == 1'b0 && debounce_counter == DELAY_COUNT) begin
-                next_state <= r; //fill in next state
+                next_state <= 2'b10; // Move to RELEASE_DETECTED state
             end else if (button == 1'b1) begin
-                next_state <= p ;//fill in next state
+                next_state <= 2'b01; // Stay in PRESSED state
             end else begin
-                next_state <= r ;//fill in next state
+                next_state <= 2'b00; // Move to IDLE state
             end
         end
 
         2'b10: begin // RELEASE_DETECTED state
             if (button == 1'b1 && debounce_counter == DELAY_COUNT) begin
-                next_state <=  i;//fill in next state
+                next_state <= 2'b00; // Move to IDLE state
             end else if (button == 1'b0) begin
-                next_state <= r; //fill in next state
+                next_state <= 2'b10; // Stay in RELEASE_DETECTED state
             end else begin
-                next_state <= i; //fill in next state
+                next_state <= 2'b01; // Move to PRESSED state
             end
         end
 
@@ -56,15 +56,15 @@ always @(posedge clk or posedge rst) begin
     endcase
 end
 
-//below is where debounced signal is outputted
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         debounced_button <= 1'b0;
     end else begin
         case (state)
-            2'b10: debounced_button <= 1'b1; //Output 1 when RELEASED
-            default: debounced_button <= 1'b0; //Output 0 otherwise
+            2'b10: debounced_button <= 1'b1; // Output 1 when RELEASE_DETECTED
+            default: debounced_button <= 1'b0; // Output 0 otherwise
         endcase
     end
 end
+
 endmodule
